b27171ebf22e2a471e7b1eb8ffdc47cbde1dd441
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-09 12:01:48 -0300

    formatters added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     def nome_aplicacao
         "CRYPTO WALLET APP"
     end
+
+    def ambiente_rails
+
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "teste"
+        end
+    end
 end
