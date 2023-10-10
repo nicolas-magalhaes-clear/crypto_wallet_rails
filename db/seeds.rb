e5cbb8ed240869a9f05cdadc2e23ff123e7bb615
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-10 09:51:59 -0300

    seeds refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+coins = [
+          {
+            description: "Dogecoin",
+            acronym: "DOGE",
+            url_image: "https://i.ibb.co/sgb17NS/pngwing-com.png"
+          },
+          {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://upload.wikimedia.org/wikipedia/commons/5/50/Bitcoin.png"
+          },
+          {
+            description: "ETHEREUM",
+            acronym: "ETH",
+            url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+          },
+        ]
 
-Coin.create!(
-    description: "Dogecoin",
-    acronym: "DOGE",
-    url_image: "https://i.ibb.co/sgb17NS/pngwing-com.png"
-)
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://upload.wikimedia.org/wikipedia/commons/5/50/Bitcoin.png"
-)
-Coin.create!(
-    description: "ETHEREUM",
-    acronym: "ETH",
-    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
-)
+
+coins.each do |coin|
+    Coin.find_or_create_by!(coin)
+end
+
+
