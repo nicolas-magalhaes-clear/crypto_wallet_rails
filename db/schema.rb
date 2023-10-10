9d0cabc8d25ef1b501e0c7fc2deeb96f7073c48b
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-10 09:47:51 -0300

    miningtype migration created

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_06_140151) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_10_124456) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
